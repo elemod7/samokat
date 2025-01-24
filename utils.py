b8608fe463e601881cae05d1276b8615db2f690f
elemod7 <156799062+elemod7@users.noreply.github.com>
2025-01-25 01:02:06 +0300

    utils.py

--- utils.py
+++ utils.py
@@ -3,11 +3,7 @@ import logging
 import phonenumbers
 from datetime import datetime
 
-
 def save_to_csv(user_id, name, phone_number, message, file_path='user_requests.csv'):
-    """
-    Сохраняет данные пользователя в CSV.
-    """
     try:
         with open(file_path, mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
@@ -15,44 +11,29 @@ def save_to_csv(user_id, name, phone_number, message, file_path='user_requests.c
     except Exception as e:
         logging.error(f"Ошибка при записи в CSV: {e}")
 
-
 def normalize_phone_number(phone: str) -> str:
-    """
-    Проверяет и нормализует номер телефона.
-    Возвращает номер в международном формате, если он валиден, иначе None.
-    """
     try:
-        # Парсим номер телефона
-        parsed_number = phonenumbers.parse(phone, "RU")  # RU — регион по умолчанию
+        parsed_number = phonenumbers.parse(phone, "RU")
         if phonenumbers.is_valid_number(parsed_number):
-            # Возвращаем номер в международном формате
             return phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
     except phonenumbers.NumberParseException:
         return None
 
-
 def read_user_ids(file_path='user_ids.csv'):
-    """
-    Читает список ID пользователей из CSV.
-    """
     user_ids = []
     try:
         with open(file_path, mode='r', encoding='utf-8') as file:
             reader = csv.reader(file)
             for row in reader:
                 try:
-                    user_ids.append(int(row[0]))  # Считаем, что ID находятся в первом столбце
+                    user_ids.append(int(row[0])) 
                 except ValueError:
                     logging.warning(f"Неверный ID пользователя: {row[0]}")
     except FileNotFoundError:
         logging.warning(f"Файл {file_path} не найден.")
     return user_ids
 
-
 def is_male_name(name: str) -> bool:
-    """
-    Определяет, является ли имя мужским.
-    """
     name = name.strip().lower()
     female_endings = ("а", "я", "ия")
     return not any(name.endswith(ending) for ending in female_endings)
